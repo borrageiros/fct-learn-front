67f909b8fdf87e6a2ab7316874bd1e6d71413584
Adrián Borrageiros Mourelos <adrian.borrageiros@netexlearning.com>
2023-04-18 15:19:06 +0200

    Profile view and nickname update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from "reactstrap";
 
-function App() {
 
-    return (
-        <div className='App'>
-          <h1>FIRST LOOK</h1>
-        </div>
-    );
-}
-  
+import Header from "./components/Header/Header";
+import Home from "./screens/Home/Home";
+import Profile from "./screens/Profile/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+// fontawesome
+import initFontAwesome from "./utils/initFontAwesome";
+initFontAwesome();
+
+const App = () => {
+  const { isLoading, error } = useAuth0();
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <BrowserRouter>
+      <div id="app" className="d-flex flex-column h-100">
+      <Header />
+      <Container className="flex-grow-1 mt-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Container>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <div className='App'>
+          <h1>FIRST LOOK</h1>
+        </div>
+    );
+}
+
+export default Home;
